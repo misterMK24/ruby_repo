9a94e9161a6469cca09f5c12a111eaf0a1807ba6
MisterMK <Aleksandr.Seleznev24@gmail.com>
2019-01-30 11:28:26 -0800

    added some classes and schema

--- test.rb
+++ test.rb
@@ -1,5 +1,15 @@
-x = 10
-until x == 21 do
-  puts "x = #{x}"
-  x += 1
+class CPinfo
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def file_open
+    File.new("/home/asd/files/cpinfo_test") do |file|
+      # some method for find out the necessary information
+    end
+  end
+
+  def topology_cluster
+    # schema for searching interfaces from topology - cluster and physical i-faces
+  end
 end

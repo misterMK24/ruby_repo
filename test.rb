ea87e5ddcd3cb2d6ac0af3c492f83aa587c45a43
misterMK24 <41155221+misterMK24@users.noreply.github.com>
2019-01-31 19:17:08 +0300

    Update test.rb
    opening file

--- test.rb
+++ test.rb
@@ -4,12 +4,22 @@ class CPinfo
   end
 
   def file_open
-    File.new("/home/asd/files/cpinfo_test") do |file|
-      # some method for find out the necessary information
-    end
+    @file_opened = File.new(@filename, "r")
   end
 
   def topology_cluster
+    @file_opened.each_line do |line|
+      File.open("C:\\_ruby\\file_output", "a+") do |newfile|
+        newfile.write(line)
+      end
+    end
+
+    @file_opened.close
     # schema for searching interfaces from topology - cluster and physical i-faces
   end
 end
+
+
+instance_1 = CPinfo.new("C:\\_ruby\\cpinfo_chepucks.info")
+instance_1.file_open
+instance_1.topology_cluster

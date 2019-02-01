2562ec25bda92d12b2b6d90ab18a8c7ac58396f8
misterMK24 <41155221+misterMK24@users.noreply.github.com>
2019-02-01 11:40:47 +0300

    got a name of the cluster

--- test.rb
+++ test.rb
@@ -10,13 +10,18 @@ class CPinfo
   end
 
   def topology_cluster
-    @file_opened.each_line.with_index do |line, index|
-      if line.include? "gw_cluster" or index
+    index = nil
+    @file_opened.each_line do |line|
+      if line.include? ":ClassName (gateway_cluster)"
+        gw_name = line.scan(%r'\(\w+\)')
+        puts "#{gw_name[0].gsub(%r'\W', "")}"
         index = true
+      elsif index
         if line.include? ":HA_mode"
           break
-        elsif line.include? ":ClassName"
-          puts "cluster name is #{line.scan(/(\w+)/)}"
+        elsif line.include? ":name"
+          some_str = line.scan(%r'\(\w+\)')
+          puts "cluster name is #{some_str[0].gsub(%r'\W', "")}"
         end
       end
     end
@@ -26,9 +31,10 @@ class CPinfo
     # schema for searching interfaces from topology - cluster and physical i-faces
   end
 
+# //home//asd//ruby_theory//cpinfo_chepucks.info - for Linux
+# C:\\_ruby\\cpinfo_chepucks.info - for Windows
 
-
-instance_1 = CPinfo.new("//home//asd//ruby_theory//cpinfo_chepucks.info")
+instance_1 = CPinfo.new("C:\\_ruby\\cpinfo_chepucks.info")
 instance_1.file_open
 instance_1.topology_cluster
 # instance_1.file_open

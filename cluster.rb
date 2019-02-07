cde9c9bf308542a1457a7803e78871f45bad0529
MisterMK <Aleksandr.Seleznev24@gmail.com>
2019-02-07 12:42:53 -0800

    modified local_interfaces method

--- cluster.rb
+++ cluster.rb
@@ -11,16 +11,17 @@ class NewClass
 
   def read_file
 
-   f = File.open(@filename, "r")
-   whole_file = f.read
-   f.close
-   return whole_file
+  f = File.open(@filename, "r")
+  whole_file = f.read
+
+  return whole_file.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
 
   end
 
   def cl_name
     
     whole_file = self.read_file
+    # puts "#{whole_file}"
     some_str_tmp = whole_file.scan(/:ClassName[\s]{1}[(]{1}gateway_cluster[)]{1}[\S\s]+?:edges/)
     some_str_tmp = some_str_tmp[0].scan(/:cluster_members[\S\s]+?:edges/)
     some_str_tmp = some_str_tmp[0].scan(/:Name[\s]{1}[(][\S\s]+?[)]/)
@@ -52,12 +53,19 @@ class NewClass
   def local_interfaces
 
     final_array = Array.new
+    some_str_tmp = Array.new(2)
     some_str = self.cl_name          # got the names of the cluster members
     whole_file = self.read_file
-    check_regex = Regexp.new(":[\s]{1}[(]+?#{cl_name[0]}[\S\s]+?:interfaces[\S\s]+?:Machine_weight")
-    puts "#{check_regex}"
-    some_str_tmp = whole_file.scan(/:[\s]{1}[(]#{self.cl_name[0]}[\S\s]+?:interfaces[\S\s]+?:Machine_weight/)
-    puts "#{some_str_tmp[0]}"
+    gw_names = self.cl_name
+    gw_names.each_with_index do |gw, index|
+      some_str_tmp[index] = whole_file.scan(/:[\s]{1}[(]#{gw}[\S\s]+?:interfaces[\S\s]+?:Machine_weight/)[0]
+    end
+
+    some_str_tmp.each do |gw| 
+      puts "#{gw}"
+    end
+    # puts "#{whole_file.scan(/:[\s]{1}[(]#{self.cl_name[0]}[\S\s]+?:interfaces[\S\s]+?:Machine_weight/)[0]}"
+    # puts "#{whole_file.scan(/:[\s]{1}[(]#{self.cl_name[1]}[\S\s]+?:interfaces[\S\s]+?:Machine_weight/)}"
 
   end
 
@@ -74,6 +82,7 @@ class NewClass
   def cluster
     # self.to_file(self.cl_name)
     # self.cl_interfaces
+    # self.cl_name
     self.local_interfaces
     # puts "#{self.cl_name[0]} \n"
   end
@@ -82,5 +91,5 @@ class NewClass
 end
 
 
-new_cluster = NewClass.new("C:\\_ruby\\cpinfo_cpmgmt-int-srv.txt")
+new_cluster = NewClass.new("//home//asd//ruby_theory//cpinfo_chepucks.info")   # aaa
 new_cluster.cluster

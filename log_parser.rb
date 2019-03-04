99ee7aee2436e8116927486e1eea5abedb4c9533
misterMK24 <41155221+misterMK24@users.noreply.github.com>
2019-03-04 17:02:57 +0300

    refactoring

--- log_parser.rb
+++ log_parser.rb
@@ -1,19 +1,27 @@
-class Log_parse
+require 'RubyXL'
 
+class Log_parse
+  attr_accessor :input_filename, :output_filename, :result_filename
 # method for getting the whole <migrate rule> lines from check point log file
-  def file_open
+  def initial_file_parsing
     match_result_array = []
     i = 0
     begin
-      File.open('C:\_ruby\log_in_ascii.txt', 'r') do |f|      # /mnt/c/_ruby/log_in_ascii.txt
-        count = f.size
-        f.each do |line|
-          check_migrate_rule = line.match(/.*migrate rule.*/)
-          if check_migrate_rule
-              match_result_array[i] = check_migrate_rule[0]
-              i += 1
-          end
-        end  
+      if @input_filename.empty?
+        return puts "Check :input_filename variable !!!"
+      else
+        puts "current filename is: #{@input_filename}"
+        File.open(@input_filename, 'r') do |f|      # /mnt/c/_ruby/log_in_ascii.txt
+          count = f.size
+          f.each do |line|
+            check_migrate_rule = line.match(/.*migrate rule.*/)
+            if check_migrate_rule
+                match_result_array[i] = check_migrate_rule[0]
+                i += 1
+            end
+          end  
+        end
+        puts "initial parsing has ended"
       end
     rescue EOFError
       # finish to read file
@@ -21,39 +29,74 @@ class Log_parse
     to_file(match_result_array) # save result to the file 
   end
 
-  def to_file(match_result_array)
-    File.open('C:\_ruby\results_s.txt', 'w') do |file|                # /mnt/c/_ruby/results.txt
-      match_result_array.each do |line|
-        file.write(line)
-        file.write("\n")
-      end
-    end
-  end
-
 # method to parse each line to get source_ip, dst_ip, proto and service.
-  def migrate_rule
-    test_array = []
+  def get_src_dst_proto_port
+    src_ip = []
+    dst_ip = []
+    proto = []
+    service_port = []
     final_array = []
-    File.open('C:\_ruby\results.txt', 'r') do |f|
+
+    File.open(@output_filename, 'r') do |f|
       f.each_with_index do |line, i|
-        test_array[0] = line.split(";")
-        final_array[i] = test_array[0][16] + "\s"
-        final_array[i] << test_array[0][17] + "\s"
-        final_array[i] << "#{test_array[0][18]} #{test_array[0][19]}"
+        test_array = line.split(";")
+        src_ip[i] = test_array[16]
+        dst_ip[i] = test_array[17]
+        proto[i] = test_array[18]
+        service_port[i] = test_array[19]
       end
     end
-    puts "end"
-    to_file(final_array)
+    puts "Done parse operation"
+    src_ip.uniq!
+    dst_ip.uniq!
+    proto.uniq!
+    service_port.uniq!.sort!
+    puts "Done uniq! operation"
+    to_excel_file(src_ip, dst_ip, proto, service_port)
   end
 
-  def save_to_db
-    
-    
-    
+  def to_file(match_result_array)
+    if @output_filename.empty?
+      return puts "Check :output_filename variable !!!"
+    else
+      File.open(@output_filename, 'w') do |file|                # /mnt/c/_ruby/results.txt
+        match_result_array.each do |line|
+          file.write(line)
+          file.write("\n")
+        end
+      end
+    end
   end
+
+  def to_excel_file(src_ip, dst_ip, proto, service_port)
+    workbook = RubyXL::Parser.parse(@result_filename)
+    worksheet = workbook.add_worksheet()
+    src_ip.each_with_index do |ip, row|
+      worksheet.add_cell(row, 0, ip)
+    end
+    
+    dst_ip.each_with_index do |ip, row|
+      worksheet.add_cell(row, 1, ip)
+    end
+
+    proto.each_with_index do |proto, row|
+      worksheet.add_cell(row, 2, proto)
+    end
+
+    service_port.each_with_index do |port, row|
+      worksheet.add_cell(row, 3, port)
+    end
+    workbook.save
+    puts "Done xlsx operation"
+  end
+
+
 
 end
 
 ins_log_parse = Log_parse.new
-# ins_log_parse.file_open
-ins_log_parse.migrate_rule          # file_open
+ins_log_parse.input_filename = 'C:\_old_PC\ГП ТГ Казань\2019\kszi_logs\01_02_2019.txt'
+ins_log_parse.output_filename = 'C:\_old_PC\ГП ТГ Казань\2019\kszi_logs\output_01_02_2019.txt'
+ins_log_parse.result_filename = 'C:\_old_PC\ГП ТГ Казань\2019\kszi_logs\results.xlsx'
+ins_log_parse.initial_file_parsing
+ins_log_parse.get_src_dst_proto_port

7d583eb087ca6806e59667d5192474e1656f645f
misterMK24 <41155221+misterMK24@users.noreply.github.com>
2019-03-05 17:56:05 +0300

    Update log_parser.rb

--- log_parser.rb
+++ log_parser.rb
@@ -2,18 +2,15 @@ require 'RubyXL'
 require 'logger'
 
 class Log_parse
-  attr_accessor :input_filename, :output_filename, :result_filename
+  attr_accessor :dir, :input_filename, :output_filename, :result_filename
 # method for getting the whole <migrate rule> lines from check point log file
   def initial_file_parsing
     match_result_array = []
     i = 0
-    begin
-      if @input_filename.empty?
-        return puts "Check :input_filename variable !!!"
-      else
-        puts "current filename is: #{@input_filename}"
-        File.open(@input_filename, 'r') do |f|      # /mnt/c/_ruby/log_in_ascii.txt
-          count = f.size
+    @input_filename.each do |filename|
+      begin
+        puts "current filename is: #{filename}"
+        File.open(@dir + filename, 'r') do |f|      # /mnt/c/_ruby/log_in_ascii.txt
           f.each do |line|
             check_migrate_rule = line.match(/.*migrate rule.*/)
             if check_migrate_rule
@@ -22,12 +19,36 @@ class Log_parse
             end
           end  
         end
-        puts "initial parsing has ended"
-      end
-    rescue EOFError
+        to_file(match_result_array)
+        match_result_array.clear
+        i = 0
+        puts "parsing for #{filename} has ended"
+      rescue EOFError
       # finish to read file
+      end
     end
-    to_file(match_result_array) # save result to the file 
+#    to_file(match_result_array)
+ #  begin
+ #    if @input_filename.empty?
+ #      return puts "Check :input_filename variable !!!"
+ #    else
+ #      puts "current filename is: #{@input_filename}"
+ #      File.open(@input_filename, 'r') do |f|      # /mnt/c/_ruby/log_in_ascii.txt
+ #        count = f.size
+ #        f.each do |line|
+ #          check_migrate_rule = line.match(/.*migrate rule.*/)
+ #          if check_migrate_rule
+ #              match_result_array[i] = check_migrate_rule[0]
+ #              i += 1
+ #          end
+ #        end  
+ #      end
+ #      puts "initial parsing has ended"
+ #    end
+ #  rescue EOFError
+ #    # finish to read file
+ #  end
+ #   to_file(match_result_array) # save result to the file 
   end
 
 # method to parse each line to get source_ip, dst_ip, proto and service.
@@ -66,7 +87,7 @@ class Log_parse
     if @output_filename.empty?
       return puts "Check :output_filename variable !!!"
     else
-      File.open(@output_filename, 'w') do |file|                # /mnt/c/_ruby/results.txt
+      File.open(@output_filename, 'a+') do |file|                # /mnt/c/_ruby/results.txt
         match_result_array.each do |line|
           file.write(line)
           file.write("\n")
@@ -102,8 +123,18 @@ class Log_parse
 end
 
 ins_log_parse = Log_parse.new
-ins_log_parse.input_filename = 'C:\_old_PC\ГП ТГ Казань\2019\kszi_logs\01_02_2019.txt'
-ins_log_parse.output_filename = 'C:\_old_PC\ГП ТГ Казань\2019\kszi_logs\output_01_02_2019.txt'
+ins_log_parse.dir = "C:\\_old_PC\\ГП ТГ Казань\\2019\\kszi_logs\\"
+ins_log_parse.input_filename = 
+[
+  '01_02_2019.txt', '05_02_2019.txt', 
+  '07_02_2019.txt', '09_02_2019.txt', 
+  '11_02_2019.txt', '13_02_2019.txt',
+  '15_02_2019.txt', '18_02_2019.txt', 
+  '20_02_2019.txt', '21_02_2019.txt', 
+  '24_02_2019.txt', '26_02_2019.txt',
+  '28_02_2019.txt'
+]
+ins_log_parse.output_filename = 'C:\_old_PC\ГП ТГ Казань\2019\kszi_logs\output_02_2019.txt'
 ins_log_parse.result_filename = 'C:\_old_PC\ГП ТГ Казань\2019\kszi_logs\results.xlsx'
-# ins_log_parse.initial_file_parsing
-ins_log_parse.get_src_dst_proto_port
+ins_log_parse.initial_file_parsing
+# ins_log_parse.get_src_dst_proto_port

acb8f9b32662e3d01707f7dd7019cd8c0a73aa03
misterMK24 <41155221+misterMK24@users.noreply.github.com>
2019-03-26 12:56:42 +0300

    Update log_parser.rb
    Add new instance variables: :src_ip_final_table, :dst_ip_final_table, :proto_final_table, :service_port_final_table.
    
    Add initialize function.
    Made refactoring for get_src_dst_proto_port function.

--- log_parser.rb
+++ log_parser.rb
@@ -2,16 +2,40 @@ require 'RubyXL'
 require 'logger'
 
 class Log_parse
-  attr_accessor :dir, :input_filename, :output_filename, :result_filename
+  attr_accessor :dir, :input_filename, :output_filename, :result_filename,
+                  :src_ip_final_table, :dst_ip_final_table, 
+                  :proto_final_table, :service_port_final_table
 # method for getting the whole <migrate rule> lines from check point log file
+  def initialize
+    self.dir = "C:\\_old_PC\\ГП ТГ Казань\\2019\\kszi_logs\\"
+    self.output_filename = []
+    self.input_filename = 
+  [
+    '01_02_2019.txt', '05_02_2019.txt', 
+    '07_02_2019.txt', '09_02_2019.txt', 
+    '11_02_2019.txt', '13_02_2019.txt',
+    '15_02_2019.txt', '18_02_2019.txt', 
+    '20_02_2019.txt', '21_02_2019.txt', 
+    '24_02_2019.txt', '26_02_2019.txt',
+    '28_02_2019.txt'
+  ]
+    self.src_ip_final_table = []
+    self.dst_ip_final_table = []
+    self.proto_final_table = []
+    self.service_port_final_table = []
+    @input_filename.each_with_index do |filename, iterator|
+      puts @dir
+      self.output_filename[iterator] = @dir + 'output_' + filename
+    end
+  end
+
   def initial_file_parsing
     match_result_array = []
-    i = 0
-    @input_filename.each do |filename|
-      self.output_filename = @dir + 'output_' + filename
+    @input_filename.each_with_index do |filename, iterator|
       begin
         puts "current filename is: #{filename}"
         File.open(@dir + filename, 'r') do |f|      # /mnt/c/_ruby/log_in_ascii.txt
+          i = 0
           f.each do |line|
             check_migrate_rule = line.match(/.*migrate rule.*/)
             if check_migrate_rule
@@ -20,84 +44,40 @@ class Log_parse
             end
           end  
         end
-        test_get_src_dst_proto_port(match_result_array)
-#        to_file(match_result_array)
+        # get_src_dst_proto_port(match_result_array)
+        # to_file(match_result_array, iterator)
         match_result_array.clear
-        i = 0
         puts "parsing for #{filename} has ended"
       rescue EOFError
       # finish to read file
       end
     end
-#    to_file(match_result_array)
   end
 
-  def test_get_src_dst_proto_port(match_result_array)
-    src_ip = []
-    dst_ip = []
-    proto = []
-    service_port = []
-    final_array = []
-    y = 0
-
-    match_result_array.each_with_index do |each_log|
-      test_array = each_log.split(",")                           
-      src_ip[i] = test_array[2]
-      dst_ip[i] = test_array[3]
-      proto[i] = test_array[4]
-      if test_array[5].eql?("\n")
-        next
-      else
-        service_port[y] = test_array[5]
-        y += 1
-      end
-    end
-    puts "Done parse operation"
-    src_ip.uniq!.sort!
-    dst_ip.uniq!.sort!
-    proto.uniq!.sort!
-    service_port_sorted = service_port.uniq!.sort_by { |element| element.to_i }
-    puts "Done uniq! operation"
-    
-  end
-  
-# method to parse each line to get source_ip, dst_ip, proto and service.
   def get_src_dst_proto_port
-    src_ip = []
-    dst_ip = []
-    proto = []
-    service_port = []
-    final_array = []
     y = 0
-
-    File.open(@output_filename, 'r') do |f|
-      f.each_with_index do |line, i|
-        test_array = line.split(",")                           
-        src_ip[i] = test_array[2]
-        dst_ip[i] = test_array[3]
-        proto[i] = test_array[4]
+    File.open(@output_filename[0], 'r') do |file|
+      file.each_with_index do |each_log, i|
+        test_array = each_log.split(",")
+        self.src_ip_final_table[i] = test_array[2]
+        self.dst_ip_final_table[i] = test_array[3]
+        self.proto_final_table[i] = test_array[4]
         if test_array[5].eql?("\n")
           next
         else
-          service_port[y] = test_array[5]
+          self.service_port_final_table[i] = test_array[5]
           y += 1
         end
       end
     end
-    puts "Done parse operation"
-    src_ip.uniq!.sort!
-    dst_ip.uniq!.sort!
-    proto.uniq!.sort!
-    service_port_sorted = service_port.uniq!.sort_by {|element| element.to_i}
-    puts "Done uniq! operation"
-    to_excel_file(src_ip, dst_ip, proto, service_port_sorted)
+    puts @src_ip_final_table.uniq!.sort!
   end
-
-  def to_file(match_result_array)
-    if @output_filename.empty?
+    
+  def to_file(match_result_array, iterator)
+    if @output_filename[iterator].empty?
       return puts "Check :output_filename variable !!!"
     else
-      File.open(@output_filename, 'a+') do |file|                # /mnt/c/_ruby/results.txt
+      File.open(@output_filename[iterator], 'a+') do |file|                # /mnt/c/_ruby/results.txt
         match_result_array.each do |line|
           file.write(line)
           file.write("\n")
@@ -127,26 +107,9 @@ class Log_parse
     workbook.save
     puts "Done xlsx operation"
   end
-
-
-
 end
 
-ins_log_parse = Log_parse.new
-ins_log_parse.dir = "C:\\_old_PC\\ГП ТГ Казань\\2019\\kszi_logs\\"
-ins_log_parse.input_filename = 
-[
-  '01_02_2019.txt', '05_02_2019.txt', 
-  '07_02_2019.txt', '09_02_2019.txt', 
-  '11_02_2019.txt', '13_02_2019.txt',
-  '15_02_2019.txt', '18_02_2019.txt', 
-  '20_02_2019.txt', '21_02_2019.txt', 
-  '24_02_2019.txt', '26_02_2019.txt',
-  '28_02_2019.txt'
-]
-
-
-# ins_log_parse.output_filename = 'C:\_old_PC\ГП ТГ Казань\2019\kszi_logs\output_02_2019.txt'
-ins_log_parse.result_filename = 'C:\_old_PC\ГП ТГ Казань\2019\kszi_logs\results.xlsx'
-ins_log_parse.initial_file_parsing
-# ins_log_parse.get_src_dst_proto_port
+instance_log_parse = Log_parse.new
+instance_log_parse.result_filename = 'C:\_old_PC\ГП ТГ Казань\2019\kszi_logs\results.xlsx'
+# ins_log_parse.initial_file_parsing
+instance_log_parse.get_src_dst_proto_port
